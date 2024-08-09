997e960e20a8574eefb040e9064179bf3994eb6c
Pangol <njseyfried@valdosta.edu>
2024-08-09 11:32:37 -0400

    bugfix cart row

--- merch.js
+++ merch.js
@@ -89,7 +89,6 @@ cartRow.innerHTML = cartRowContents
 cartItems.append(cartRow)
 cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
 cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
-{alert('Item Added to Cart')}
 }
 
 
