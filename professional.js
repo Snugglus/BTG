a884ac9c3f18916c035db74bd5afac1e55f338ca
Snugglus <138148034+Snugglus@users.noreply.github.com>
2024-08-19 14:15:59 -0400

    Professionals Update
    Updated API retrieval and Array prep framework

--- professional.js
+++ professional.js
@@ -1,4 +1,26 @@
-fetch("https://raider.io/api/v1/mythic-plus/affixes?region=us&locale=e")
+const url = "https://raider.io/api/v1/mythic-plus/affixes?region=us&locale=en";
+const members = "https://raider.io/api/v1/guilds/profile?region=us&realm=%22Bleeding%20Hollow%22&name=%20the%20blacktooth%20grin&fields=members";
+
+
+fetch(url)
     .then(response => response.json(response))
-    .then(data => console.log(data))
-    .catch(errir => console.error(error));
+    .then(data => {
+        const affixesArray = data;
+        const affixDetails = affixesArray['affix_details'];
+        let affixCards = affixDetails.map((
+            { name, description, icon }) => ({ name, description, icon }));
+        console.log(affixCards);
+    })
+
+fetch(members)
+    .then(response => response.json(response))
+    .then(data=> {
+        const membersArray = data;
+        const membersOnly = membersArray['members'];
+        console.log(membersOnly);
+    })
+
+
+
+
+
